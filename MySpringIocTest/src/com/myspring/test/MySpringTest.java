45c9c161b6c124d1765a940fe930288cecd88105
aabbcc5050 <aabbcc5050@qq.com>
2018-04-01 22:23:15 +0800

    idea git test

--- MySpringIocTest/src/com/myspring/test/MySpringTest.java
+++ MySpringIocTest/src/com/myspring/test/MySpringTest.java
@@ -6,9 +6,10 @@ import com.myspring.controller.UserController;
 import com.myspring.core.MySpringBeans;
 import com.myspring.entity.User;
 
+
 public class MySpringTest {
 
-	
+	//测试是否生效
 	public static void main(String[] args) throws Exception {
 		MySpringBeans msb=new MySpringBeans();
 		UserController userControlelr=(UserController) msb.getBeanByClassName("UserController");
